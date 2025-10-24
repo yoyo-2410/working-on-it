bc95e2b52f2b6dc96afaeb8adf0d278f7e5ed2c3
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2025-10-24 22:46:16 +0530

    MERGE SORT.py

--- MERGE SORT.py
+++ MERGE SORT.py
@@ -1,23 +1,15 @@
 def merge_sort(a):
-    if len(a) <= 1:
-        return a
+    if len(a) <= 1: return a
     mid = len(a)//2
-    left = merge_sort(a[:mid])
-    right = merge_sort(a[mid:])
-    return merge(left, right)
-
-def merge(L, R):
+    L, R = merge_sort(a[:mid]), merge_sort(a[mid:])
     res, i, j = [], 0, 0
     while i < len(L) and j < len(R):
-        if L[i] < R[j]:
-            res.append(L[i]); i += 1
-        else:
-            res.append(R[j]); j += 1
-    res += L[i:] + R[j:]
-    return res
+        res.append(L[i] if L[i] < R[j] else R[j])
+        if L[i] < R[j]: i += 1
+        else: j += 1
+    return res + L[i:] + R[j:]
 
 print("MERGE SORT METHOD\n")
-data = [int(x) for x in input("Enter elements separated by space: ").split()]
-print("\nUnsorted Array:", data)
-sorted_data = merge_sort(data)
-print("\nSorted Array using Merge Sort:", sorted_data)
+a = list(map(int, input("Enter elements separated by space: ").split()))
+print("\nUnsorted:", a)
+print("Sorted:", merge_sort(a))
