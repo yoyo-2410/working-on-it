5f09a2293d77117f8bf117e89dbf79db5f936b76
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2025-10-25 19:26:01 +0530

    age limit.py

--- age limit.py
+++ age limit.py
@@ -2,12 +2,12 @@ from datetime import datetime
 
 dob = input("Enter DOB (DD-MM-YYYY): ")
 try:
-    d = datetime.strptime(dob, '%d-%m-%Y')
+    d = datetime.strptime(dob, "%d-%m-%Y")
     t = datetime.today()
-    age = t.year - d.year - ((t.month,t.day)<(d.month,d.day))
+    age = t.year - d.year - ((t.month, t.day) < (d.month, d.day))
     print("Age:", age)
-    if age<18: print("Underage")
-    elif age<60: print("Working age")
-    else: print("Retirement age")
+    if age < 18: print("Underage, below 18")
+    elif age < 60: print("Within working age limit")
+    else: print("At or above retirement age")
 except:
-    print("Invalid date format")
+    print("Invalid date format! Use DD-MM-YYYY.")
