69411dc42c5bce5434ec94fa215cbaf24c54521f
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2025-10-25 19:24:46 +0530

    Minimum Spanning Tree.py

--- Minimum Spanning Tree.py
+++ Minimum Spanning Tree.py
@@ -1,25 +1,18 @@
-V = 5
-G = [
-    [0, 4, 0, 5, 2],
-    [4, 0, 1, 3, 0],
-    [0, 1, 0, 8, 0],
-    [5, 3, 8, 0, 2],
-    [2, 0, 0, 2, 0]
-]
+V=5
+G=[[0,4,0,5,2],
+   [4,0,1,3,0],
+   [0,1,0,8,0],
+   [5,3,8,0,2],
+   [2,0,0,2,0]]
 
-selected = [0]*V
-selected[0] = 1
-edges = 0
+sel=[0]*V; sel[0]=1; e=0
 print("\nEdge : Weight")
-
-while edges < V-1:
-    min_w, x, y = 9999, 0, 0
+while e<V-1:
+    m,x,y=9999,0,0
     for i in range(V):
-        if selected[i]:
+        if sel[i]:
             for j in range(V):
-                if not selected[j] and G[i][j]:
-                    if G[i][j] < min_w:
-                        min_w, x, y = G[i][j], i, j
+                if not sel[j] and G[i][j] and G[i][j]<m:
+                    m,x,y=G[i][j],i,j
     print(f"{x}-{y}: {G[x][y]}")
-    selected[y] = 1
-    edges += 1
+    sel[y]=1; e+=1
