fadb25ea06749a7c9ae7dcebdddbea29b0bbfa28
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2025-10-25 19:19:24 +0530

    2D Matrix Operations.py

--- 2D Matrix Operations.py
+++ 2D Matrix Operations.py
@@ -1,15 +1,19 @@
 import numpy as np
 
-r1,c1 = map(int,input().split())
-r2,c2 = map(int,input().split())
+r1, c1 = map(int, input("Enter rows, cols of Matrix 1: ").split())
+r2, c2 = map(int, input("Enter rows, cols of Matrix 2: ").split())
 
-A = np.array([list(map(float,input().split())) for _ in range(r1)])
-B = np.array([list(map(float,input().split())) for _ in range(r2)])
+A = np.array([list(map(float, input("Row: ").split())) for _ in range(r1)])
+B = np.array([list(map(float, input("Row: ").split())) for _ in range(r2)])
 
-if r1==r2 and c1==c2: print("Add:\n",A+B,"\nSub:\n",A-B)
-else: print("Add/Sub not possible")
+if r1 == r2 and c1 == c2:
+    print("\nAddition:\n", A + B)
+    print("Subtraction:\n", A - B)
+else:
+    print("\nAddition/Subtraction not possible.")
 
-if c1==r2: print("Mul:\n",A.dot(B))
-else: print("Mul not possible")
+if c1 == r2: print("\nMultiplication:\n", A @ B)
+else: print("\nMultiplication not possible.")
 
-s = float(input()); print("Scaled:\n",A*s)
+s = float(input("\nEnter scalar: "))
+print("\nScaling:\n", A * s)
