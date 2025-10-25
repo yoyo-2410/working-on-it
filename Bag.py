a70690d25884408ffeaedaf5f8280f3b67932d82
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2025-10-25 19:15:59 +0530

    Bag.py

--- Bag.py
+++ Bag.py
@@ -1,19 +1,11 @@
-# Program: Bag
-
 class Bag:
-    def __init__(self):
-        self.items = []
+    def __init__(self): self.items = []
+    def add(self, i): self.items.append(i)
+    def contains(self, v): return v in self.items
 
-    def add(self, item):
-        self.items.append(item)
+b = Bag()
+for _ in range(int(input("Enter number of items: "))):
+    b.add(input("Enter item: "))
 
-    def count(self, item):
-        return self.items.count(item)
-
-bag = Bag()
-bag.add(10)
-bag.add(20)
-bag.add(10)
-
-print("Bag items:", bag.items)
-print("Count of 10:", bag.count(10))
+v = input("Enter value to search: ")
+print("Found!" if b.contains(v) else "Not Found!")
