fd21fd26d87286af7dd74186651fd2d09c73e21d
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2025-10-25 19:20:31 +0530

    infix to postfix using Stack ADT.py

--- infix to postfix using Stack ADT.py
+++ infix to postfix using Stack ADT.py
@@ -2,24 +2,21 @@ class Stack:
     def __init__(self): self.s=[]
     def push(self,x): self.s.append(x)
     def pop(self): return self.s.pop() if self.s else ''
-    def peek(self): return self.s[-1] if self.s else ''
+    def top(self): return self.s[-1] if self.s else ''
     def empty(self): return not self.s
 
-prec = lambda op: {'+':1,'-':1,'*':2,'/':2,'^':3}.get(op,0)
-isop = lambda ch: ch.isalnum()
-
-def infix_to_postfix(expr):
-    s, out = Stack(), ''
-    for ch in expr:
-        if isop(ch): out+=ch
-        elif ch=='(': s.push(ch)
+def infix_to_postfix(exp):
+    p, st, out = {'+':1,'-':1,'*':2,'/':2,'^':3}, Stack(), ''
+    for ch in exp:
+        if ch.isalnum(): out += ch
+        elif ch=='(': st.push(ch)
         elif ch==')':
-            while s.peek()!='(': out+=s.pop()
-            s.pop()
+            while st.top()!='(': out+=st.pop()
+            st.pop()
         else:
-            while not s.empty() and prec(ch)<=prec(s.peek()): out+=s.pop()
-            s.push(ch)
-    while not s.empty(): out+=s.pop()
+            while not st.empty() and p.get(ch,0)<=p.get(st.top(),0): out+=st.pop()
+            st.push(ch)
+    while not st.empty(): out+=st.pop()
     return out
 
 print("Postfix:", infix_to_postfix(input("Enter infix: ")))
